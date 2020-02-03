341fca50af5b323a4427e7a5680c5ebae92f992b
Marcos Couto <marcos_otuoc@hotmail.com>
2020-02-02 22:32:45 -0300

    First commit

---
+++ src/main/java/com/marcoscouto/brazilianleague/BrazilianleagueApplication.java
@@ -0,0 +1,13 @@
+package com.marcoscouto.brazilianleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrazilianleagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrazilianleagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcoscouto/brazilianleague/BrazilianleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcoscouto.brazilianleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrazilianleagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
